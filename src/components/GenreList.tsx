a1ca44b467cb3913f5e27dc863729d628aae4488
girirajsinhgohil <gvgohil232@gmail.com>
2024-07-27 16:42:33 +0530

    Create a Generic data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,14 @@
-import useGenres from "../hooks/useGenres"
+import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-    const { genres } = useGenres();
-    return (
-        <ul>
-            {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
-        </ul>
-    )
-}
+  const { data } = useGenres();
+  return (
+    <ul>
+      {data.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
 
-export default GenreList
+export default GenreList;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "3af6dc47f14e424e89c4e651a190ae9a",
+        key: "c542e67aec3a4340908f9de9e86038af",
     }
 })
